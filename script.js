f13f85265e4c04a3dd10dfe8574cebbdb87230fd
iqromulmuslimin51-maker <iqromulmuslimin51@gmail.com>
2026-08-16 09:01:27 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
+
+// menuIcon.onclick = () => {
+//     menuIcon.classList.toggle('bx-x')
+//     navbar.classList.toggle('active');
+// }
+
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x')
+    menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+};
+
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.onclick = () => {
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+    };
+});
